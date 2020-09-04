e840dec9e3f43c2dfc0e1da275db2e0aadbb7aa0
satyajitrpatnaik <bvr.satyajit.patnaik@gmail.com>
2020-09-04 20:30:44 +0530

    Add middleware - redux-logger

--- experiments/vanilla-redux/index.js
+++ experiments/vanilla-redux/index.js
@@ -1,6 +1,10 @@
 const redux = require('redux');
+const reduxLogger = require('redux-logger');
+
 const createStore = redux.createStore;
 const combineReducers = redux.combineReducers;
+const logger = reduxLogger.createLogger();
+const applyMiddleware = redux.applyMiddleware;
 
 console.log('Logged from Index.js!');
 
@@ -18,7 +22,7 @@ const rootReducer = combineReducers({
 });
 
 // store
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 // Getting the current value of the rdux store using getState()
 console.log('Initially the redux store has value: ' + JSON.stringify(store.getState()));
