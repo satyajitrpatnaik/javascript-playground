b132581543352ea2beb3c14f78474f73cc3c6667
satyajitrpatnaik <bvr.satyajit.patnaik@gmail.com>
2020-09-04 16:25:33 +0530

    Add a full flow of updating and subsribing a redux store using actions and reducers

--- experiments/vanilla-redux/index.js
+++ experiments/vanilla-redux/index.js
@@ -1,1 +1,51 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
 console.log('Logged from Index.js!');
+
+// action type
+const BUY_CAKE = 'BUY_CAKE';
+
+// action creator
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    boughtAmount: 1,
+    info: 'Buying Cake'
+  }
+}
+
+// reducer
+const initialState = {
+  numberOfCakes: 10
+};
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numberOfCakes: state.numberOfCakes - 1
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+// Getting the current value of the rdux store using getState()
+console.log('Initially the redux store has value: ' + JSON.stringify(store.getState()));
+
+// Setting subscribers to any change in values in the redux store
+// subscribe() method returns back a function which can be used to deregister the listener/subscriber
+const unsubscribe = store.subscribe(() => {
+  console.log('Updated state: ', store.getState());
+});
+
+// invoking actions
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+
+unsubscribe();
