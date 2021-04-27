4d6dd6be493829ddd74fa9fd5c72981598dccee0
satyajitrpatnaik <bvr.satyajit.patnaik@gmail.com>
2021-04-27 16:23:47 +0530

    use of React Refresh for preserving state while changing HTML/JSX during development

--- experiments/react-webpack-ts/src/App.tsx
+++ experiments/react-webpack-ts/src/App.tsx
@@ -1,6 +1,7 @@
 import "./style.css";
 import IMAGE from "./react.png";
 import LOGO from "./react-svg.svg";
+import { ClickCounter } from "./ClickCounter";
 
 export const App = () => {
   return (
@@ -10,6 +11,7 @@ export const App = () => {
       </h1>
       <img src={IMAGE} alt="React logo" width="300" height="200" />
       <img src={LOGO} alt="React logo" width="300" height="200" />
+      <ClickCounter />
     </>
   );
 };
---
+++ experiments/react-webpack-ts/src/ClickCounter.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export const ClickCounter = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <button onClick={() => setCount((prev) => prev + 1)}>
+        Count -> {count}
+      </button>
+    </div>
+  );
+};
